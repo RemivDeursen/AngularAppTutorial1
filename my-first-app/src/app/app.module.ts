374757cff48d36f219df7202dc3efeeef6c28338
Remi <remivdeursen@gmail.com>
2020-03-02 14:06:48 +0100

    Initial Commit

---
+++ my-first-app/src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule} from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon';
+
+@NgModule({
+   imports: [
+      BrowserModule,
+      ReactiveFormsModule,
+      MatIconModule,
+      RouterModule.forRoot([
+        { path: '', component: ProductListComponent },
+        { path: 'products/:productId', component: ProductDetailsComponent},
+      ]),
+      BrowserAnimationsModule
+   ],
+   declarations: [
+      AppComponent,
+      ProductListComponent,
+      TopBarComponent,
+      ProductAlertsComponent,
+      ProductDetailsComponent
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
+})
+export class AppModule { }
